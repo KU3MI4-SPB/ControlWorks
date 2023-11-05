1dbed14446e9234d9144fc414b1d12b421001ec0
KU3MI4 <kuzmichov@bk.ru>
2023-11-05 12:50:28 +0300

    add toyshop class in control work 3

---
+++ Control_Work_3/ToyShop.java
@@ -0,0 +1,42 @@
+package Control_Work_3;
+
+import java.util.PriorityQueue;
+import java.util.Random;
+
+public class ToyShop {
+    private PriorityQueue<Toy> toyQueue;
+
+    public ToyShop() {
+        toyQueue = new PriorityQueue<>();
+    }
+
+    public void addToy(Toy toy) {
+        toyQueue.add(toy);
+    }
+
+    public int getRandomToyId() {
+        Random random = new Random();
+        int randomNumber = random.nextInt(10);
+
+        int cumulativeWeight = 0;
+        for (Toy toy : toyQueue) {
+            cumulativeWeight += toy.getFrequency();
+            if (randomNumber < cumulativeWeight) {
+                return toy.getId();
+            }
+        }
+
+        // Если все игрушки имеют нулевую частоту выпадения, вернуть -1
+        return -1;
+    }
+
+    public Toy getToyById(int randomToyId) {
+        for (Toy toy : toyQueue) {
+            if (toy.getId() == randomToyId) {
+                return toy;
+            }
+        }
+        return null;
+    }
+}
+
